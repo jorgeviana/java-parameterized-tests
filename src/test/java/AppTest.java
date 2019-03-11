2fbba3d4912a87fd9dfdfaf4d86f583eb7b57f35
Jorge Viana <secoviana@gmail.com>
2019-03-11 18:00:12 +0100

    junit 5 parameterized tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,41 +1,20 @@
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.Arrays;
-import java.util.Collection;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
-
-@RunWith(Parameterized.class)
 public class AppTest {
 
-    @Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-//              { a, b, max},
-                { 1, 1, 1 },
-                { 1, 9, 9 },
-                { 8, 2, 8 },
-                { 9, 9, 9 },
-        });
-    }
-
-    private int a;
-    private int b;
-    private int max;
-
-    public AppTest(int a, int b, int max) {
-        this.a = a;
-        this.b = b;
-        this.max = max;
-    }
-
-    @Test
-    public void test() {
-        assertThat(Math.max(a, b), equalTo(max));
+    @ParameterizedTest
+    @CsvSource({
+//           a, b, max
+            "1, 1, 1",
+            "1, 9, 9",
+            "8, 2, 8",
+            "9, 9, 9"
+    })
+    void test(int a, int b, int max) {
+        assertEquals(max, Math.max(a, b));
     }
 
 }
